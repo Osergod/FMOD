37aff4e86eb9dc88742adb9cf33bba299757572c
Alejandro Baldeon <a24josbalesp@inspedralbes.cat>
2025-12-14 00:02:33 +0100

    Ultimos cambios, todo funcionable

--- Assets/Scripts/VCAController.cs
+++ Assets/Scripts/VCAController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VCAController : MonoBehaviour
 {
@@ -10,11 +11,31 @@ public class VCAController : MonoBehaviour
     private VCA musicVCA;
     private VCA sfxVCA;
 
+    [SerializeField] private Slider generalSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
     void Start()
     {
+        // Obtener referencias a los VCAs
         generalVCA = RuntimeManager.GetVCA("vca:/GeneralVCA");
         musicVCA = RuntimeManager.GetVCA("vca:/MusicVCA");
         sfxVCA = RuntimeManager.GetVCA("vca:/SFXVCA");
+
+        // Inicializar sliders al 50%
+        generalSlider.value = 0.5f;
+        musicSlider.value = 0.5f;
+        sfxSlider.value = 0.5f;
+
+        // Aplicar valores iniciales a FMOD
+        SetGeneralVolume(0.5f);
+        SetMusicVolume(0.5f);
+        SetSFXVolume(0.5f);
+
+        // Conectar eventos de los sliders
+        generalSlider.onValueChanged.AddListener(SetGeneralVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     public void SetGeneralVolume(float value) => generalVCA.setVolume(value);
